aaedec02ca231252ff5a18864e424bead16d4828
Apil Tiwari <twintiwari@gmail.com>
2019-02-16 14:43:22 -0600

    api gate way day third

--- orderms/src/main/java/com/edureka/orderms/repository/OrderRepository.java
+++ orderms/src/main/java/com/edureka/orderms/repository/OrderRepository.java
@@ -2,11 +2,12 @@ package com.edureka.orderms.repository;
 
 import com.edureka.orderms.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByUserId(Long userId);
+    List<Order> findByUserId(@Param("userId") Long userId);
 }
